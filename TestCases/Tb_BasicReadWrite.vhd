b6bbdea1730ee35bea100ad8fe73e26c97712315
Jim Lewis <jim@synthworks.com>
2025-04-26 11:12:46 -0700

    Update test case and naming

--- TestCases/Tb_BasicReadWrite.vhd
+++ TestCases/Tb_BasicReadWrite.vhd
@@ -1,5 +1,5 @@
 --
---  File Name:         TbRegister_BasicReadWrite.vhd
+--  File Name:         Tb_BasicReadWrite.vhd
 --  Design Unit Name:  Architecture of TestCtrl
 --  Revision:          OSVVM MODELS STANDARD VERSION
 --
@@ -50,7 +50,7 @@ begin
   ControlProc : process
   begin
     -- Initialization of test
-    SetTestName("TbRegister_BasicReadWrite") ;
+    SetTestName("Tb_BasicReadWrite") ;
     SetLogEnable(PASSED, TRUE) ;    -- Enable PASSED logs
     SetLogEnable(INFO, TRUE) ;    -- Enable INFO logs
 
@@ -61,13 +61,10 @@ begin
 
     -- Wait for Design Reset
     wait until nReset = '1' ;  
-    ClearAlerts ;
+    -- ClearAlerts ;
 
     -- Wait for test to finish
-    WaitForBarrier(OsvvmTestDone, 35 ms) ;
-  --  AlertIf(now >= 35 ms, "Test finished due to timeout") ;  -- now part of EndOfTestReports
-  --  AlertIf(GetAffirmCount < 1, "Test is not Self-Checking"); -- Reporting Checks This
-    
+    WaitForBarrier(OsvvmTestDone, 35 ms) ;    
     
     TranscriptClose ; 
     -- Printing differs in different simulators due to differences in process order execution
@@ -83,54 +80,89 @@ begin
   --   Generate transactions for AxiManager
   ------------------------------------------------------------
   ManagerProc : process
+    variable Addr : std_logic_vector(ADDR_WIDTH-1 downto 0) := ADDR_ZERO ;
     variable Data : std_logic_vector(DATA_WIDTH-1 downto 0) ;
+    constant TEST_ADDR_INCREMENT : integer := 16#20# ;
   begin
     wait until nReset = '1' ;  
     WaitForClock(ManagerRec, 2) ; 
-    log("Write and Read with ByteAddr = 0, 4 Bytes") ;
-    Write(ManagerRec,   X"0000_0010", X"5555_5555" ) ;
-    Read(ManagerRec,    X"0000_0010", Data) ;
+    
+    log("Write, Read, ReadCheck words, 4 Bytes") ;
+    Addr := ADDR_ZERO ; 
+    Write(ManagerRec,   Addr, X"5555_5555" ) ;
+    Read(ManagerRec,    Addr, Data) ;
     AffirmIfEqual(Data, X"5555_5555", "Manager Read Data: ") ;
-    
-    log("Write and Read with 1 Byte, and ByteAddr = 0, 1, 2, 3") ; 
-    Write(ManagerRec, X"0000_0020", X"11" ) ;
-    Write(ManagerRec, X"0000_0021", X"22" ) ;
-    Write(ManagerRec, X"0000_0022", X"33" ) ;
-    Write(ManagerRec, X"0000_0023", X"44" ) ;
 
-    Read(ManagerRec,    X"0000_0020", Data) ;
-    AffirmIfEqual(Data, X"4433_2211", "Manager Read Data: ") ;
-    
-    Read(ManagerRec,  X"0000_0020", Data(7 downto 0)) ;
-    AffirmIfEqual(Data(7 downto 0), X"11", "Manager Read Data: ") ;
-    Read(ManagerRec,  X"0000_0021", Data(7 downto 0)) ;
-    AffirmIfEqual(Data(7 downto 0), X"22", "Manager Read Data: ") ;
-    Read(ManagerRec,  X"0000_0022", Data(7 downto 0)) ;
-    AffirmIfEqual(Data(7 downto 0), X"33", "Manager Read Data: ") ;
-    Read(ManagerRec,  X"0000_0023", Data(7 downto 0)) ;
-    AffirmIfEqual(Data(7 downto 0), X"44", "Manager Read Data: ") ;
 
+    log("Read with 1 Byte, and ByteAddr = 0, 1, 2, 3") ; 
+    Addr := Addr + WORD_ADDR_INCREMENT ;
+    Write    (ManagerRec, Addr, X"A4A3A2A1" ) ;
+    ReadCheck(ManagerRec, Addr, X"A4A3A2A1" ) ;
+
+    Read(ManagerRec,  Addr,     Data(7 downto 0)) ;
+    AffirmIfEqual(Data(7 downto 0), X"A1", "Manager Read Data: ") ;
+    Read(ManagerRec,  Addr + 1, Data(7 downto 0)) ;
+    AffirmIfEqual(Data(7 downto 0), X"A2", "Manager Read Data: ") ;
+    Read(ManagerRec,  Addr + 2, Data(7 downto 0)) ;
+    AffirmIfEqual(Data(7 downto 0), X"A3", "Manager Read Data: ") ;
+    Read(ManagerRec,  Addr + 3, Data(7 downto 0)) ;
+    AffirmIfEqual(Data(7 downto 0), X"A4", "Manager Read Data: ") ;
+
+    log("Read with 2 Bytes, and ByteAddr = 0, 1, 2") ; 
+    Addr := Addr + WORD_ADDR_INCREMENT ;
+    Write    (ManagerRec, Addr, X"B4B3B2B1" ) ;
+    Read(ManagerRec,  Addr,     Data(15 downto 0)) ;
+    AffirmIfEqual(Data(15 downto 0), X"B2B1", "Manager Read Data: ") ;
+    Read(ManagerRec,  Addr + 1, Data(15 downto 0)) ;
+    AffirmIfEqual(Data(15 downto 0), X"B3B2", "Manager Read Data: ") ;
+    Read(ManagerRec,  Addr + 2, Data(15 downto 0)) ;
+    AffirmIfEqual(Data(15 downto 0), X"B4B3", "Manager Read Data: ") ;
+
+    log("Read with 3 Bytes, and ByteAddr = 0, 1") ; 
+    Addr := Addr + WORD_ADDR_INCREMENT ;
+    Write    (ManagerRec, Addr, X"C4C3C2C1" ) ;
+    Read(ManagerRec,  Addr,     Data(23 downto 0)) ;
+    AffirmIfEqual(Data(23 downto 0), X"C3C2C1", "Manager Read Data: ") ;
+    Read(ManagerRec,  Addr + 1, Data(23 downto 0)) ;
+    AffirmIfEqual(Data(23 downto 0), X"C4C3C2", "Manager Read Data: ") ;
+
+    log("Write and ReadCheck with 1 Byte, and ByteAddr = 0, 1, 2, 3") ; 
+    Addr := ADDR_ZERO + TEST_ADDR_INCREMENT ; 
+    Write(ManagerRec, Addr,     X"D1" ) ;
+    Write(ManagerRec, Addr + 1, X"D2" ) ;
+    Write(ManagerRec, Addr + 2, X"D3" ) ;
+    Write(ManagerRec, Addr + 3, X"D4" ) ;
+
+    Read(ManagerRec,    Addr, Data) ;
+    AffirmIfEqual(Data, X"D4D3_D2D1", "Manager Read Data: ") ;
     
---    log("Write and Read with 2 Bytes, and ByteAddr = 0, 1, 2") ;
---    Write(ManagerRec, X"BBBB_BBB0", X"2211" ) ;
---    Write(ManagerRec, X"BBBB_BBB1", X"33_22" ) ;
---    Write(ManagerRec, X"BBBB_BBB2", X"4433" ) ;
---
---    Read(ManagerRec,  X"1111_1110", Data(15 downto 0)) ;
---    AffirmIfEqual(Data(15 downto 0), X"BBAA", "Manager Read Data: ") ;
---    Read(ManagerRec,  X"1111_1111", Data(15 downto 0)) ;
---    AffirmIfEqual(Data(15 downto 0), X"CCBB", "Manager Read Data: ") ;
---    Read(ManagerRec,  X"1111_1112", Data(15 downto 0)) ;
---    AffirmIfEqual(Data(15 downto 0), X"DDCC", "Manager Read Data: ") ;
---
---    log("Write and Read with 3 Bytes and ByteAddr = 0. 1") ;
---    Write(ManagerRec, X"CCCC_CCC0", X"33_2211" ) ;
---    Write(ManagerRec, X"CCCC_CCC1", X"4433_22" ) ;
---
---    Read(ManagerRec,  X"1111_1110", Data(23 downto 0)) ;
---    AffirmIfEqual(Data(23 downto 0), X"CC_BBAA", "Manager Read Data: ") ;
---    Read(ManagerRec,  X"1111_1111", Data(23 downto 0)) ;
---    AffirmIfEqual(Data(23 downto 0), X"DDCC_BB", "Manager Read Data: ") ;
+    ReadCheck(ManagerRec,  Addr,     X"D1") ;
+    ReadCheck(ManagerRec,  Addr + 1, X"D2") ;
+    ReadCheck(ManagerRec,  Addr + 2, X"D3") ;
+    ReadCheck(ManagerRec,  Addr + 3, X"D4") ;
+
+    log("Write and Read with 2 Bytes, and ByteAddr = 0, 1, 2") ; 
+    Addr := ADDR_ZERO + 2*TEST_ADDR_INCREMENT ; 
+    Write(ManagerRec, Addr,     X"1211" ) ;
+    Addr := Addr + WORD_ADDR_INCREMENT ;
+    Write(ManagerRec, Addr + 1, X"23_22" ) ;
+    Addr := Addr + WORD_ADDR_INCREMENT ;
+    Write(ManagerRec, Addr + 2, X"3433" ) ;
+
+    Addr := ADDR_ZERO + 2*TEST_ADDR_INCREMENT ; 
+    ReadCheck(ManagerRec, Addr,     X"1211" ) ;
+    Addr := Addr + WORD_ADDR_INCREMENT ;
+    ReadCheck(ManagerRec, Addr + 1, X"23_22" ) ;
+    Addr := Addr + WORD_ADDR_INCREMENT ;
+    ReadCheck(ManagerRec, Addr + 2, X"3433" ) ;
+
+    log("Write and Read with 3 Bytes and ByteAddr = 0. 1") ;
+    Addr := ADDR_ZERO + 3*TEST_ADDR_INCREMENT ; 
+    Write(ManagerRec, Addr,                           X"43_4241" ) ;
+    Write(ManagerRec, Addr + WORD_ADDR_INCREMENT + 1, X"5453_52" ) ;
+
+    ReadCheck(ManagerRec, Addr,                           X"43_4241" ) ;
+    ReadCheck(ManagerRec, Addr + WORD_ADDR_INCREMENT + 1, X"5453_52" ) ;
     
     -- Wait for outputs to propagate and signal OsvvmTestDone
     WaitForClock(ManagerRec, 2) ;
@@ -138,13 +170,12 @@ begin
     wait ;
   end process ManagerProc ;
 
-
 end BasicReadWrite ;
 
-Configuration TbRegister_BasicReadWrite of TbRegister is
-  for TestHarness
+Configuration Tb_BasicReadWrite of TestHarness is
+  for Structural
     for TestCtrl_1 : TestCtrl
       use entity work.TestCtrl(BasicReadWrite) ; 
     end for ; 
   end for ; 
-end TbRegister_BasicReadWrite ; 
+end Tb_BasicReadWrite ; 
